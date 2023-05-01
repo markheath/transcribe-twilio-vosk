2cbfe42fce9d1842cf2822664761000043be6c28
Mark Heath <mark.heath@gmail.com>
2023-05-01 11:51:08 +0100

    configure forwarded headers and web sockets

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<ForwardedHeadersOptions>(
+    options => options.ForwardedHeaders = ForwardedHeaders.All
+);
+
 var app = builder.Build();
+app.UseForwardedHeaders();
+app.UseWebSockets();
 
 app.MapGet("/", () => "Hello World!");
 
