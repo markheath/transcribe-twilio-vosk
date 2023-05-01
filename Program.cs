5bbdd7e6d1cdbb5e8bfc06a6a3f5ea072260a2f4
Mark Heath <mark.heath@gmail.com>
2023-05-01 14:36:31 +0100

    Configure the vosk recognizer

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.HttpOverrides;
+using Vosk;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<ForwardedHeadersOptions>(
@@ -8,6 +9,16 @@ builder.Services.Configure<ForwardedHeadersOptions>(
 var app = builder.Build();
 app.UseForwardedHeaders();
 app.UseWebSockets();
+
+// You can set to -1 to disable logging messages
+Vosk.Vosk.SetLogLevel(0);
+var model = new Model("model");
+var spkModel = new SpkModel("model-spk");
+var rec = new VoskRecognizer(model, 16000.0f);
+rec.SetSpkModel(spkModel);
+rec.SetMaxAlternatives(0);
+rec.SetWords(true);
+
 
 app.MapGet("/", () => "Hello World!");
 
