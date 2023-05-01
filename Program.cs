3a82d451032929adc11bcbf094e9c5a71aa53f23
Mark Heath <mark.heath@gmail.com>
2023-05-01 14:53:54 +0100

    add the /voice endpoint

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.HttpOverrides;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
 using Vosk;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,5 +24,14 @@ rec.SetWords(true);
 
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/voice", (HttpRequest request) =>
+{
+    var response = new VoiceResponse();
+    var connect = new Connect();
+    connect.Stream(url: $"wss://{request.Host}/stream");
+    response.Append(connect);
+    return Results.Extensions.TwiML(response);
+});
 
 app.Run();
